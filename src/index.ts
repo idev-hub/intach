1f9ab7f5d23d5b9796ceda88cb31bbe5ae1cde10
idev-hub <7vg7tek7sG>
2020-08-29 14:12:10 +0500

    update project

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 require('dotenv').config()
 
+
+import express = require("express")
 import chgpgt from "./bot"
 import {users} from './database'
 import './scenes/scenes'
 import './commands/commands'
+
 
 users.asyncLoadDatabase().then(() => {
     console.log('INFO: local database loaded successfully.')
@@ -11,12 +14,16 @@ users.asyncLoadDatabase().then(() => {
     console.log('FATAL: local database could not be loaded. Caused by: ' + e)
 })
 
-const http = require("http");
 
-http.createServer(function (request, response) {
-    response.end("InTach");
-}).listen(process.env.PORT || 80, () => {
-    console.log("INFO: Server host")
+const app: express.Application = express()
+const port: String | Number | undefined = process.env.PORT || 80
+
+app.get("*", function (req, res) {
+    res.send("<h1><a href='https://vk.com/in_teach'>InTach</a></h1>")
+})
+
+app.listen(port, function () {
+    console.log(`INFO: App is listening on port ${port}!`)
     chgpgt.updates.startPolling().then(() => {
         console.log("INFO: BOT RUNNING")
     }).catch(console.error)
