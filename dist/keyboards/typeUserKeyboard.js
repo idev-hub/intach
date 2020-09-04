0d0f882911ca102fe40824a5931264d281edfb3c
idev-hub <7vg7tek7sG>
2020-09-04 06:04:29 +0500

    update project

--- dist/keyboards/typeUserKeyboard.js
+++ dist/keyboards/typeUserKeyboard.js
@@ -3,14 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vk_io_1 = require("vk-io");
 exports.default = vk_io_1.Keyboard.builder().textButton({
     label: "Ученик",
-    payload: {
-        command: 0
-    },
     color: vk_io_1.Keyboard.POSITIVE_COLOR
 }).textButton({
     label: "Учитель",
-    payload: {
-        command: 1
-    },
     color: vk_io_1.Keyboard.PRIMARY_COLOR
 }).oneTime();
--- src/keyboards/typeUserKeyboard.ts
+++ src/keyboards/typeUserKeyboard.ts
@@ -2,14 +2,8 @@ import {Keyboard} from "vk-io";
 
 export default Keyboard.builder().textButton({
     label: "Ученик",
-    payload: {
-        command: 0
-    },
     color: Keyboard.POSITIVE_COLOR
 }).textButton({
     label: "Учитель",
-    payload: {
-        command: 1
-    },
     color: Keyboard.PRIMARY_COLOR
 }).oneTime()
