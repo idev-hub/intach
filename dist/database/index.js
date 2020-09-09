1ab7995ecb992a747a6b60af8415a3908b6b8be1
idev-hub <7vg7tek7sG>
2020-09-09 07:05:55 +0500

    update project

--- dist/database/index.js
+++ dist/database/index.js
@@ -2,8 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 let sequelize;
-sequelize = new sequelize_1.Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
-    host: process.env.DBHOST,
-    dialect: "mysql",
-});
+if (process.env.NODE_ENV === "development") {
+    sequelize = new sequelize_1.Sequelize({
+        host: process.env.DBHOST,
+        username: process.env.DBNAME,
+        password: process.env.DBPASSWORD,
+        database: process.env.DBUSER,
+        dialect: "mysql",
+    });
+}
+else {
+    sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            }
+        },
+        ssl: true,
+    });
+}
 exports.default = sequelize;
