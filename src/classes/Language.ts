6a1862179415e2b1bd7f11255a8de691713eb0c5
idev-hub <7vg7tek7sG>
2020-10-15 00:53:10 +0500

    update project2

--- src/classes/Language.ts
+++ src/classes/Language.ts
@@ -1,20 +1,23 @@
 import ru from "../lang/ru"
 import zh from "../lang/zh"
-
+import ua from "../lang/ua"
 
 export default class Language {
     public lang: string
     public readonly ru: object
     public readonly zh: object
+    public readonly ua: object
 
     constructor(context, lang?: string) {
         this.lang = lang ? lang : ((context.user) ? context.user.peer.lang : (context.session.lang ? context.session.lang : "ru")) || "ru"
         this.ru = ru
         this.zh = zh
+        this.ua = ua
     }
 
     public readonly template = (): object => {
         if (this.lang === "zh") return this.zh
+        else if (this.lang === "ua") return this.ua
         else return this.ru
     }
 }
